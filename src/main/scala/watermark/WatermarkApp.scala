3ed96bb6292c34136c0d69667c78d5b37697069d
Maftunbek <Prince777_98@mail.ru>
2024-06-05 21:38:13 +0500

    small fix

--- src/main/scala/watermark/WatermarkApp.scala
+++ src/main/scala/watermark/WatermarkApp.scala
@@ -18,8 +18,13 @@ object WatermarkApp {
     // Define the watermark text and its properties
     val watermarkText = "Bobur Go'ja"
     val discountText = "50%"
-    val watermarkFont = FontUtils.createFont("Arial", image.width * 3 / 100)
-    val discountFont = FontUtils.createFont("Arial", image.width * 8 / 100)
+    val salePriceText = "8000"
+    val uzsText = "UZS"
+    val watermarkFont = FontUtils.createFont("Arial", image.width * 4 / 100)
+    val discountFont = FontUtils.bold("Arial", image.width * 14 / 200)
+    val salePriceFont = FontUtils.bold("Arial", image.width * 14 / 300)
+    val uzsFont = FontUtils.bold("Arial", image.width * 7 / 300)
+    val discountColor = new RGBColor(255, 0, 0) // White color with some transparency
     val watermarkColor = new RGBColor(255, 255, 255) // White color with some transparency
 
     // Calculate the position to place the watermark (bottom right corner)
@@ -29,25 +34,43 @@ object WatermarkApp {
       new Text(
         watermarkText,
         image.width - (image.width * 7 / 9),
-        image.height - (image.height * 9 / 160),
+        image.height - (image.height * 1 / 20),
         g => {
           g.setColor(watermarkColor)
           g.setFont(watermarkFont)
         },
       ),
       new Text(
-        discountText,
+        salePriceText,
         image.width - (image.width * 39 / 40),
-        image.height - (image.height * 7 / 80),
+        image.height - (image.height * 1 / 10),
         g => {
           g.setColor(watermarkColor)
+          g.setFont(salePriceFont)
+        },
+      ),
+      new Text(
+        uzsText,
+        image.width - (image.width * 69 / 80),
+        image.height - (image.height * 1 / 10),
+        g => {
+          g.setColor(watermarkColor)
+          g.setFont(uzsFont)
+        },
+      ),
+      new Text(
+        discountText,
+        image.width - (image.width * 3 / 16),
+        image.height - (image.height * 69 / 80),
+        g => {
+          g.setColor(discountColor)
           g.setFont(discountFont)
         },
       )
     )
 
     // Save the watermarked image
-    implicit val writer: JpegWriter = new JpegWriter().withCompression(100)
+    implicit val writer: JpegWriter = new JpegWriter()//.withCompression(100)
     val outputPath = "./watermarked-image.jpg"
     watermarkedImage.output(writer, new java.io.File(outputPath))
 
